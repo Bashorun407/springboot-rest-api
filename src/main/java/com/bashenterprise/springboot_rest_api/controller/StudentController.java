f71d8ab7d3ee2d8d1373bafd16510017f846cef8
Bashorun Olusheyi <boboladvat@gmail.com>
2025-02-22 11:00:09 +0100

    Committing method annotated with @RequestParam to local repo

--- src/main/java/com/bashenterprise/springboot_rest_api/controller/StudentController.java
+++ src/main/java/com/bashenterprise/springboot_rest_api/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.bashenterprise.springboot_rest_api.controller;
 import com.bashenterprise.springboot_rest_api.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -23,6 +24,7 @@ public class StudentController {
     return student;
     }
 
+    //http://localhost:8080/students
     @GetMapping("/students")
     public List<Student> getStudents(){
         List<Student> students = new ArrayList<>();
@@ -39,13 +41,24 @@ public class StudentController {
 
     //SPRING BOOT REST API with PATH VARIABLE Annotation
     //Path variable binds method argument to URI template
-    //URI template {id}
+    //http://localhost:8080/student/1/Femi/Shodimu
 
     @GetMapping("/student/{id}/{first-name}/{last-name}")
     public Student studentPathVariable(
             @PathVariable int id,
             @PathVariable("first-name") String firstName,
             @PathVariable("last-name") String lastName){
+        return new Student(id, firstName, lastName);
+    }
+
+    //SPRING BOOT APPLICATION with REQUESTPARAM Annotation
+    //RequestParam extracts query parameters from HTTP GET Request
+    //http://localhost:8080/student/query?id=1&firstName=Bolaji&lastName=Kashamu
+
+    @GetMapping("/student/query")
+    public Student getStudentRequestParam(@RequestParam int id,
+                                          @RequestParam String firstName,
+                                          @RequestParam String lastName){
         return new Student(id, firstName, lastName);
     }
 }
